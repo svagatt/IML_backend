e3d859dc1780cf1c10ae2db2c72069ceaf770a37
Varsha <gattuvarsha@gmail.com>
2021-11-26 16:58:39 +0100

    chore: update gitignore

--- ica.py
+++
@@ -1,9 +0,0 @@
-from mne.preprocessing import ICA
-components = 25
-random = 23
-decim = 3
-def fast_ica(raw):
-    ica = ICA(n_components=components, random_state=random)
-    print(ica)
-    ica.fit(raw, picks='eeg', decim=decim)
-    ica.plot_components()
