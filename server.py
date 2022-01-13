e2523ad613dfcb081bcd72e852e26d191de3c73f
Varsha <gattuvarsha@gmail.com>
2022-01-13 15:04:24 +0100

    fix: keyboard interrupt bug fix

--- server.py
+++ server.py
@@ -7,9 +7,14 @@ socket.bind('tcp://*:5555')
 
 
 async def get_request():
-    message = await socket.recv_string()
-    print(f'Request Received: {message}')
-    return message
+    messages = await socket.recv_multipart()
+    messages_str = []
+    for message in messages:
+        messages_str.append(message.decode('utf-8'))
+        print(f'Request Received: {message}')
+    complete_message =' '.join(messages_str)
+    print(complete_message)
+    return complete_message
 
 
 async def action_based_on_request(repsocket, request_message):
@@ -24,26 +29,35 @@ async def action_based_on_request(repsocket, request_message):
         await asyncio.sleep(2)
         await repsocket.send_string('World')
         print('response sent')
+    elif request_message == 'Hello again':
+        await asyncio.sleep(2)
+        await repsocket.send_string('World Again')
+        print('response sent')
+    elif 'RequestToCorrectLabel' in request_message:
+        label = request_message.split()[-1]
+        await asyncio.sleep(2)
+        await repsocket.send_string('Label corrected')
     elif request_message == 'RequestToRetrainModel':
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
         #TODO: Add the online training method file
         await repsocket.send_string('Retrained')
         print('response to retrain sent')
 
 
-async def main():
+def main():
     try:
         while True:
-            message = await get_request()
-            await action_based_on_request(socket, message)
+            message = asyncio.run(get_request())
+            asyncio.run(action_based_on_request(socket, message))
     except KeyboardInterrupt:
         print('User triggered exit')
         socket.close(linger=0)
         context.term()
         raise SystemExit
 
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
 
 
 
